190bc7133b2a75627aa48166ef0ac5d14d975967
Erik <3salaz.dev@gmail.com>
2023-05-17 19:46:07 -0700

    added auth login signUp and logout with firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+import AuthDetails from './components/Auth/AuthDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We are in development</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn/>
+      <SignUp/>
+      <AuthDetails/>
     </div>
   );
 }
